afbeb6d19f02f16f96ef0d0ca870803ceba4cc37
devdrops-com <shin.chan.of.choi.choi@gmail.com>
2023-10-12 17:18:23 +0900

    1 file modified

--- index.js
+++ index.js
@@ -4,21 +4,4 @@ import './style.css';
 // Write Javascript code!
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
-$(function () {
-  // $('p').css('color', 'red');
-  // $('.entry-page p:first-child').text('変更できたよ');
-  // $('ol li:nth-of-type(even)').css('color', 'red');
-  $('#dark-button').parents('.entry-page').find('h1').css('color', 'red');
-});
-
-$('.entry-list').on('click', function () {
-  $(this).next().toggle();
-});
-
-$('#dark-button').on('click', function () {
-  $(this).parents('.entry-page').addClass('entry-page--is-dark');
-  $(this)
-    .parents('.entry-page')
-    .find('h1')
-    .after('<p>ダークモードにするときは部屋を明るくしてね</p>');
-});
+$(function () {});
